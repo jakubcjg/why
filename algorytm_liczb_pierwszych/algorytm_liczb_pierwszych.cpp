6d7c06643bc85f53be5bc6ed7d57e033998a8a32
jakubcjg <zwykly.nowak.2137@wp.pl>
2022-03-17 08:50:23 +0100

    updated 170322

--- algorytm_liczb_pierwszych/algorytm_liczb_pierwszych.cpp
+++ algorytm_liczb_pierwszych/algorytm_liczb_pierwszych.cpp
@@ -1,10 +1,29 @@
 ﻿#include <iostream>
-
+#include <conio.h>
 using namespace std;
-
 int main()
 {
-	int p = 2;
-	int d = 0;
-	int ld = 
+    int a;
+    cout << "Ile liczb pierwszych chcesz wyswietlic? : ";
+    cin >> a;
+    int b = 2;
+    if (a >= b)
+    {
+        cout << b << endl;
+        b++;
+        for (int d = 3; a >= d; d++)
+        {
+            cout << b << endl;
+            b++;
+            for (int c = 1; c < b - 1; c++)
+            {
+                if (!(b % c))
+                {
+                    b++;
+                    d++;
+                }
+            }
+        }
+    }
+    _getch();
 }
